9dcea8b9ef57bd360b5f9c8e2b872367c2d5026e
Bala_Bhuvan_Varma <balabhuvanvarma@gmail.com>
2026-06-02 06:40:09 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0], ans = 0;
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] > buy){
+                ans = max(ans, prices[i] - buy);
+            }
+            else{
+                buy = prices[i];
+            }
+        }
+        return ans;
+    }
+};
